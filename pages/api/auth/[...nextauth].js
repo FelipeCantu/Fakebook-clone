01475419142f01e582c1430773ab939e2c658a44
Felipe <cantujr.felipe@gmai.com>
2022-04-01 17:57:26 -0400

    facebook auth :fire:

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import FacebookProvider from "next-auth/providers/facebook";
+
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,7 +9,12 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET
     })
+    
     // ...add more providers here
   ],
 })
